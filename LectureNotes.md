9c9c099deb0ac9bef250bd5552b38031f7dc8062
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-16 12:29:27 -0500

    updates

---
+++ LectureNotes.md
@@ -0,0 +1,9 @@
+- Best Practice IS opinion. It changes a lot, it's just a "good idea" for now
+- Remember, always optimize for readability (ie, for the next developer)
+- We can't use static deployment sites (like netlify, and github pages)
+- Get your code to work locally, THEN deploy (don't fall into the trap of getting something deployed as soon as possible)
+- git push origin master => origin means "where you pulled from"
+- git remote gives you the text origin
+- git remote -v => tells you what branch you're on
+
+## Steps
--- api/server.js
+++ api/server.js
@@ -9,15 +9,15 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-    try {
-        const shouts = await db('shouts');
-        res.status(200).json({messageOfTheDay: process.env.MOTD, shouts })
-    } catch (error) {
-        console.error('\nERROR', error);
-        res.status(500).json({ error: 'Cannot retrieve the shouts' })
-    }
-})
+// server.get('/', async (req, res) => {
+//     try {
+//         const shouts = await db('shouts');
+//         res.status(200).json({messageOfTheDay: process.env.MOTD, shouts })
+//     } catch (error) {
+//         console.error('\nERROR', error);
+//         res.status(500).json({ error: 'Cannot retrieve the shouts' })
+//     }
+// })
 
 
 server.use('/api', apiRouter);
