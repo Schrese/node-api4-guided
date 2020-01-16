2a38fa551be6d7ddd5d3af969a65eb43b3e3182d
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-16 11:43:48 -0500

    Now will it work?

--- api/server.js
+++ api/server.js
@@ -2,10 +2,23 @@ const express = require('express');
 const helmet = require('helmet');
 
 const apiRouter = require('./api-router.js');
+const db = require('../data/db.js')
 
 const server = express();
 
 server.use(helmet());
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+    try {
+        const shouts = await db('shouts');
+        res.status(200).json({messageOfTheDay: process.env.MOTD, shouts })
+    } catch (error) {
+        console.error('\nERROR', error);
+        res.status(500).json({ error: 'Cannot retrieve the shouts' })
+    }
+})
+
 
 server.use('/api', apiRouter);
 
